0d3772a053895690efa1e13c48da4254c22ddf64
elzorap <66724137+elzorap@users.noreply.github.com>
2022-05-21 07:32:53 +0300

    Update CatImage.java

--- app/src/main/java/com/example/android/cats/CatImage.java
+++ app/src/main/java/com/example/android/cats/CatImage.java
@@ -1,36 +1,18 @@
 package com.example.android.cats;
 
-import com.google.gson.annotations.SerializedName;
-
 import java.io.Serializable;
 
 public class CatImage implements Serializable {
-    @SerializedName("id")
     private final String id;
-    @SerializedName("width")
-    private final int width;
-    @SerializedName("height")
-    private final int height;
-    @SerializedName("url")
     private final String url;
 
-    public CatImage(String id, int width, int height, String url) {
+    public CatImage(String id, String url) {
         this.id = id;
-        this.width = width;
-        this.height = height;
         this.url = url;
     }
 
     public String getId() {
         return id;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
     }
 
     public String getUrl() {
